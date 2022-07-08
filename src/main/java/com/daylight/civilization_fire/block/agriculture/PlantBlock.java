2d2f0de6ad53a49775d572a8f76409c626f769e4
HeckerPowered <1752832598@qq.com>
2022-07-08 18:56:04 +0800

    Fix warning

--- src/main/java/com/daylight/civilization_fire/block/agriculture/PlantBlock.java
+++ src/main/java/com/daylight/civilization_fire/block/agriculture/PlantBlock.java
@@ -2,27 +2,14 @@ package com.daylight.civilization_fire.block.agriculture;
 
 import com.daylight.civilization_fire.registry.BlockEntityRegistry;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.Connection;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientGamePacketListener;
-import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.LockCode;
-import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -37,24 +24,22 @@ import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.registries.ForgeRegistries;
+
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Random;
-
 
 public class PlantBlock extends BaseEntityBlock {
 
-
     public enum PlantModel {
-        DestroyModel,//直接打破模式
+        DestroyModel, //直接打破模式
         PickingModel,//摘下模式
     }
 
     public static final IntegerProperty GROW_UP_STATE = IntegerProperty.create("state", 0, 7);//生长阶段
-
 
     //方块属性
     public ResourceLocation fruitID;
@@ -64,7 +49,8 @@ public class PlantBlock extends BaseEntityBlock {
     public String[] plantBlocks;//所能种植的方块
     public int roundItem;
 
-    public PlantBlock(PlantModel plantModel, ResourceLocation fruitID, int stageLevel, float matureTick, String[] plantBlocks,int roundItem) {
+    public PlantBlock(PlantModel plantModel, ResourceLocation fruitID, int stageLevel, float matureTick,
+            String[] plantBlocks, int roundItem) {
         super(Properties.of(Material.PLANT).noCollission().strength(0));
         this.fruitID = fruitID;
         this.plantModel = plantModel;
@@ -75,8 +61,6 @@ public class PlantBlock extends BaseEntityBlock {
         this.registerDefaultState(this.stateDefinition.any().setValue(GROW_UP_STATE, 0));
 
     }
-
-    
 
     public RenderShape getRenderShape(BlockState p_49232_) {
         return RenderShape.MODEL;
@@ -90,7 +74,7 @@ public class PlantBlock extends BaseEntityBlock {
     //处理一下直接破坏类型的植物
     @Override
     public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player,
-                                       boolean willHarvest, FluidState fluid) {
+            boolean willHarvest, FluidState fluid) {
         PlantBlockEntity plantBlockEntity = (PlantBlockEntity) level.getBlockEntity(pos);
         if (plantBlockEntity != null) {
             if (plantModel != PlantModel.PickingModel && plantBlockEntity.growingState == this.stageLevel) {
@@ -102,7 +86,8 @@ public class PlantBlock extends BaseEntityBlock {
 
     //处理一下摘取类型的植物
     @Override
-    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand,
+            BlockHitResult pHit) {
         //判断一下是否为摘取类型
         PlantBlockEntity plantBlockEntity = (PlantBlockEntity) pLevel.getBlockEntity(pPos);
         if (plantBlockEntity != null) {
@@ -112,22 +97,20 @@ public class PlantBlock extends BaseEntityBlock {
                 giveFruitItem(pPlayer);
             }
         }
-        if(!pLevel.isClientSide() && pHand == InteractionHand.MAIN_HAND) {
-            ((PlantBlock) pState.getBlock()).growUpState(pLevel, pPos, Math.min(plantBlockEntity.growingState + 1, this.stageLevel));
+        if (!pLevel.isClientSide() && pHand == InteractionHand.MAIN_HAND) {
+            ((PlantBlock) pState.getBlock()).growUpState(pLevel, pPos,
+                    Math.min(plantBlockEntity.growingState + 1, this.stageLevel));
             System.out.println(plantBlockEntity.growingState + " tick:" + plantBlockEntity.growingTick);
         }
         return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
     }
 
-
     //给予果实物品
     public void giveFruitItem(Player pPlayer) {
-        ItemStack fruitStack = new ItemStack(Registry.ITEM.get(this.fruitID));
+        ItemStack fruitStack = new ItemStack(ForgeRegistries.ITEMS.getHolder(fruitID).get().value());
         fruitStack.setCount(new Random().nextInt(roundItem) + 1);
         pPlayer.addItem(fruitStack);
     }
-
-
 
     //生长一个阶段
     public void growUpState(Level level, BlockPos pos, int state) {
@@ -135,14 +118,14 @@ public class PlantBlock extends BaseEntityBlock {
         PlantBlockEntity plantBlockEntity = (PlantBlockEntity) level.getBlockEntity(pos);
         plantBlockEntity.growingState = state;
         plantBlockEntity.growingTick = (int) (this.matureTick * ((this.stageLevel + 0.0F) / (this.stageLevel - state)));
-        level.setBlock(pos,blockState.setValue(GROW_UP_STATE,state),Block.UPDATE_ALL);
+        level.setBlock(pos, blockState.setValue(GROW_UP_STATE, state), Block.UPDATE_ALL);
         level.gameEvent(GameEvent.BLOCK_PLACE, pos);
     }
 
-    public List<Block> getPlantBlock(){
+    public List<Block> getPlantBlock() {
         List<Block> plantList = new ArrayList<>();
-        for(String str : this.plantBlocks){
-            plantList.add(Registry.BLOCK.get(new ResourceLocation(str)));
+        for (String str : this.plantBlocks) {
+            plantList.add(ForgeRegistries.BLOCKS.getHolder(new ResourceLocation(str)).get().value());
         }
         return plantList;
     }
@@ -150,12 +133,14 @@ public class PlantBlock extends BaseEntityBlock {
     @Nullable
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
-        return new PlantBlockEntity(pos,state);
+        return new PlantBlockEntity(pos, state);
     }
 
     //tick
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState blockState, BlockEntityType<T> entityType) {
-        return createTickerHelper(entityType, BlockEntityRegistry.PLANT_BLOCK_ENTITY.get(), level.isClientSide ? null : PlantBlockEntity::serverTick);
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState blockState,
+            BlockEntityType<T> entityType) {
+        return createTickerHelper(entityType, BlockEntityRegistry.PLANT_BLOCK_ENTITY.get(),
+                level.isClientSide ? null : PlantBlockEntity::serverTick);
     }
 
     //方块实体 储存成长数据
@@ -169,7 +154,8 @@ public class PlantBlock extends BaseEntityBlock {
         }
 
         //tick进行刷新
-        public static void serverTick(Level level, BlockPos pos, BlockState blockState, PlantBlockEntity plantBlockEntity) {
+        public static void serverTick(Level level, BlockPos pos, BlockState blockState,
+                PlantBlockEntity plantBlockEntity) {
             BlockState state = level.getBlockState(pos.below());//获取下方方块
             PlantBlock plantBlock = ((PlantBlock) blockState.getBlock());
             if (plantBlockEntity.stopGrowingTick < 500) {
@@ -180,12 +166,18 @@ public class PlantBlock extends BaseEntityBlock {
                     int add = 1;
                     if (plantBlock.getPlantBlock().contains(state.getBlock())) {
                         add += 1 + random.nextInt(5);
-                    } else add = random.nextInt(2);
-                    if (add > 1) plantBlockEntity.growingTick += add;
-                    else plantBlockEntity.stopGrowingTick += 1;
+                    } else
+                        add = random.nextInt(2);
+                    if (add > 1)
+                        plantBlockEntity.growingTick += add;
+                    else
+                        plantBlockEntity.stopGrowingTick += 1;
                     //判断是否到达了阶段性时期
-                    if (plantBlock.matureTick / plantBlock.stageLevel * plantBlockEntity.growingState > plantBlockEntity.growingTick && plantBlock.stageLevel > plantBlockEntity.growingState) {
-                        level.setBlock(pos, blockState.setValue(GROW_UP_STATE, plantBlockEntity.growingState + 1), Block.UPDATE_ALL);
+                    if (plantBlock.matureTick / plantBlock.stageLevel
+                            * plantBlockEntity.growingState > plantBlockEntity.growingTick
+                            && plantBlock.stageLevel > plantBlockEntity.growingState) {
+                        level.setBlock(pos, blockState.setValue(GROW_UP_STATE, plantBlockEntity.growingState + 1),
+                                Block.UPDATE_ALL);
                         level.gameEvent(GameEvent.BLOCK_PLACE, pos);
                     }
 
@@ -208,9 +200,9 @@ public class PlantBlock extends BaseEntityBlock {
 
         protected void saveAdditional(CompoundTag compoundTag) {
             super.saveAdditional(compoundTag);
-            compoundTag.putInt("stopGrowingTick",this.stopGrowingTick);
-            compoundTag.putInt("growingState",this.growingState);
-            compoundTag.putInt("growingTick",this.growingTick);
+            compoundTag.putInt("stopGrowingTick", this.stopGrowingTick);
+            compoundTag.putInt("growingState", this.growingState);
+            compoundTag.putInt("growingTick", this.growingTick);
         }
 
         //服务端处理（好像多此一举了
@@ -218,7 +210,6 @@ public class PlantBlock extends BaseEntityBlock {
         public void onDataPacket(Connection net, ClientboundBlockEntityDataPacket pkt) {
             this.handleUpdateTag(Objects.requireNonNull(pkt.getTag()));
         }
-
         @Override
         public CompoundTag getUpdateTag() {
             CompoundTag compoundTag = super.getUpdateTag();
@@ -227,13 +218,11 @@ public class PlantBlock extends BaseEntityBlock {
             compoundTag.putInt("growingTick",this.growingTick);
             return compoundTag;
         }
-
         @Nullable
         @Override
         public Packet<ClientGamePacketListener> getUpdatePacket() {
             return ClientboundBlockEntityDataPacket.create(this);
         }
-
         @Override
         public void handleUpdateTag(CompoundTag nbt) {
             this.stopGrowingTick = nbt.getInt("stopGrowingTick");
