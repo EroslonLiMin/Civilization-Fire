d349b55940274dd4ca8e41c678daba8966c81267
HeckerPowered <1752832598@qq.com>
2022-07-09 17:09:14 +0800

    Change package name

---
+++ src/main/java/com/daylight/civilization_fire/common/content/recipe/CookingRecipe.java
@@ -0,0 +1,24 @@
+package com.daylight.civilization_fire.common.content.recipe;
+
+import com.daylight.civilization_fire.common.content.item.agriculture.PlantItem;
+import com.daylight.civilization_fire.common.content.item.cooking.CondimentItem;
+
+//烹饪合成配方
+public class CookingRecipe {
+    public PlantItem[] needCookingItems;
+    public CondimentItem[] needCondimentItems;
+    public String name;//配方名称
+
+    public CookingRecipe() {
+    }
+
+    public CookingRecipe setNeedCookingItems(PlantItem... plantItems) {
+        this.needCookingItems = plantItems;
+        return this;
+    }
+
+    public CookingRecipe setNeedCondimentItems(CondimentItem... condimentItems) {
+        this.needCondimentItems = condimentItems;
+        return this;
+    }
+}
