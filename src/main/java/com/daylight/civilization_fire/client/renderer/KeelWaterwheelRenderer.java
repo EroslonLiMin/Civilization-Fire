58c398f00a9f1637589c364230010e87cf749b91
HeckerPowered <1752832598@qq.com>
2022-07-17 12:16:20 +0800

    Fix warning

--- src/main/java/com/daylight/civilization_fire/client/renderer/KeelWaterwheelRenderer.java
+++ src/main/java/com/daylight/civilization_fire/client/renderer/KeelWaterwheelRenderer.java
@@ -1,9 +1,7 @@
 package com.daylight.civilization_fire.client.renderer;
 
 import com.daylight.civilization_fire.client.model.KeelWaterwheelModel;
-import com.daylight.civilization_fire.client.model.MiningBotModel;
 import com.daylight.civilization_fire.common.content.entity.agriculture.KeelWaterwheelEntity;
-import com.daylight.civilization_fire.common.content.entity.bot.MiningBot;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
